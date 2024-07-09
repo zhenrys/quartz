1239124fe6687fe15783280f222094682982f1c4
zhenrys <zhr88135033@163.com>
2024-07-09 21:48:00 +0800

    Quartz sync: Jul 9, 2024, 9:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,8 +1,5 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-import { defineComponent, h, onMounted } from 'vue';
-import Valine from 'valine';
-
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -51,30 +48,3 @@ export const defaultListPageLayout: PageLayout = {
   ],
   right: [],
 }
-
-
-export default defineComponent({
-  setup() {
-    onMounted(() => {
-      new Valine({
-        el: '#vcomments', // 挂载Valine的容器ID
-        appId: 'dEUw9Q5hFYMJDGoUErRAtD7V-gzGzoHsz',
-        appKey: 'CRsrs1NovZnoOopOTQRSBu6J',
-        placeholder: '在这里输入评论...',
-        path: window.location.pathname, // 使用当前页面的路径作为评论的唯一标识
-      });
-    });
-
-    return () => h('div', { class: 'layout' }, [
-      h('header', { class: 'header' }, 'Header Content'),
-      h('div', { class: 'container' }, [
-        h('aside', { class: 'sidebar' }, 'Sidebar Content'),
-        h('main', { class: 'main' }, [
-          h('div', {}, 'Main Content'),
-          h('div', { id: 'vcomments' }), // Valine评论系统的挂载点
-        ]),
-      ]),
-      h('footer', { class: 'footer' }, 'Footer Content'),
-    ]);
-  },
-});
